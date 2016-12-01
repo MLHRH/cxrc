a20ade73c8ea076a57ed98c6fdc5ba8de9bff1b6
jianglei01 <lei.jiang@autodesk.com>
2016-12-01 19:28:34 +0800

    []修改专家打分，专家档案

--- MLRHR/web/view/userBaseInfor/js/expertUserBase.js
+++ MLRHR/web/view/userBaseInfor/js/expertUserBase.js
@@ -1,160 +1,80 @@
-function initExpertBase() {
-	checkEdit();
-	// 基础信息提交
-	$("#submit_base").live("click", function() {
-				$("#detail_base").validate({
-							highlight : function(label) {
-								$(label).closest('.control-group').addClass('error');
-							},
-							unhighlight : function(label) {
-								$(label).closest('.control-group').removeClass('error');
-							},
-							errorElement : 'span',
-							submitHandler : function() {
-								arrData = new Array();
-								arrData.push({
-											"name" : "id",
-											"value" : $("#user_id").val()
-										});
-								arrData.push({
-											"name" : "name",
-											"value" : $("#user_name").val()
-										});
-								arrData.push({
-											"name" : "id_num",
-											"value" : $("#user_id_num").val()
-										});
-								arrData.push({
-											"name" : "body_id",
-											"value" : $("#user_body_id_select").val()
-										});
-//			
-								arrData.push({
-											"name" : "email",
-											"value" : $("#user_email").val()
-										});
-								$.ajax({
-											dataType : "json",
-											type : "post",
-											url : "updateExpert",
-											data : arrData,
-											error : function() {// 请求失败处理函数
-												alert('请求失败');
-											},
-											success : function(rst) {
-												// if(rst.result==1){
-												// noty({
-												// text: '基础信息 更新完成.请继续完善其他信息.',
-												// type: 'success',
-												// dismissQueue: false,
-												// closeWith:
-												// ['click','button'],
-												// timeout: 1500,
-												// layout: 'top',
-												// callback:{afterClose:function(){
-												// //
-												// location.href='${ctx}/userBaseInfor?bt=base';
-												// }},
-												// theme: 'defaultTheme'
-												// });
-												// }
-												// else{
-												// generatenoty('center',rst.msg,'error');
-												// }
-											}
-										});
-								arrData = new Array();
-								arrData.push({
-											"name" : "id",
-											"value" : $("#userbaseinfor_id").val()
-										});
-								arrData.push({
-											"name" : "user_id",
-											"value" : $("#user_id").val()
-										});
-							
-								arrData.push({
-											"name" : "gender",
-											"value" : $("input[name='user_gender']:checked").val()
-										});
-								if ($("#user_birthday").val() != "")
-									arrData.push({
-												"name" : "birthday",
-												"value" : $("#user_birthday").val()
-											});						
-					
-								arrData.push({
-											"name" : "minzu",
-											"value" : $("#user_minzu").val()
-										});
-								if ($("#user_canjiashijian").val() != "")
-									arrData.push({
-												"name" : "canjiashijian",
-												"value" : $("#user_canjiashijian").val()
-											});
-								arrData.push({
-											"name" : "congshizhuanye",
-											"value" : $("#user_congshizhuanye").val()
-										});
-								arrData.push({
-											"name" : "within_beijing",
-											"value" : $("#within_beijing").val()
-										});
-
-								arrData.push({
-											"name" : "xingzhengzhiwu",
-											"value" : $("#user_xingzhengzhiwu").val()
-										});
-
-								arrData.push({
-											"name" : "zhicheng",
-											"value" : $("#user_current_zhicheng").val()
-										});
-								$.ajax({
-											dataType : "json",
-											type : "post",
-											url : "updateExpertBaseDetail",
-											data : arrData,
-											error : function() {// 请求失败处理函数
-												alert('请求失败');
-											},
-											success : function(rst) {
-												if (rst.result == 1) {
-													noty({
-																text : '基础信息 更新完成.请继续完善其他信息.',
-																type : 'success',
-																dismissQueue : false,
-																closeWith : ['click', 'button'],
-																timeout : 1500,
-																layout : 'top',
-																callback : {
-																	afterClose : function() {
-																		// location.href='${ctx}/userBaseInfor?bt=base';
-																	}
-																},
-																theme : 'defaultTheme'
-															});
-												} else {
-													generatenoty('center', rst.msg, 'error');
-												}
-											}
-										});
-							}
-						});
-			});
-}
-function checkEdit() {
+var group_id ;
+function update() {
+	arrData = {};
+	arrData.birthday = $('#user_birthday').val();
+	arrData.gender = $('#user_sex').val();
+	arrData.minzu = $('#user_minzu').val();
+	arrData.congshizhuanye = $('#user_congshizhuanye').val();
+	arrData.zhicheng = $('#user_current_zhicheng').val();
+	arrData.xingzhengzhiwu = $('#user_xingzhengzhiwu').val();
+	arrData.within_beijing = $('#within_beijing').val();
+	arrData.address = $('#user_address').val();
+	arrData.group_id = group_id;
+	var baseInfo = JSON.stringify(arrData);
 	$.ajax({
-				type : 'get',
-				dataType : 'json',
-				url : 'checkEdit',// 请求的路径
-				error : function() {// 请求失败处理函数
-				},
-				success : function(rst) { // 请求成功后处理函数。
-					if (rst.result == 0) {
-						alert('您的申请记录正在审批流程中，当前状态信息不可编辑！');
-						location.href = 'zhichengApplylist';
-					}
-				}
-			});
-}
+		dataType : "json",
+		type : "post",
+		url : "baseEdit",
+		data :{"data": baseInfo},
+		error : function() {// 请求失败处理函数
+			alert('请求rrrrrr失败');
+		},
+		success : function(rst) {
+			if (rst.result == 1) {
+				alert("success!");
+
+			} else {
+				generatenoty('center', rst.msg, 'error');
+			}
+		}
+	});
+	
+	}
+var objUser = new Object();
+var contentName = "expertBase";
+$(document).ready(function() {
+	initExpertBase();
+});
+
+	function initExpertBase() {
+		$.ajax({
+					type : 'get',
+					dataType : 'json',
+					url : 'expertBase',// 请求的路径
+					error : function() {// 请求失败处理函数
+						alert('请求失败');
+					},
+					success : function(data) { // 请求成功后处理函数。
+						var myDate = new Date();
+						var year = myDate.getFullYear();
+						var month = myDate.getMonth() + 1;
+						var day = myDate.getDate();
+						$('#currentDate').html(year + "年" + month + "月" + day + "日");
+					
+						$('#user_birthday').val(data[1].birthday);
+						$('#user_sex').val(data[1].gender);
+						$('#user_minzu').val(data[1].minzu);
+					    $('#user_congshizhuanye').val(data[1].congshizhuanye);
+						$('#user_xingzhengzhiwu').val(data[1].xingzhengzhiwu);
+						$('#user_current_zhicheng').val(data[1].zhicheng);
+						$('#within_beijing').val(data[1].within_beijing);
+						$('#xingzhengzhiwu').val(data[1].xingzhengzhiwu);
+						$('#user_address').val(data[1].address);
+						group_id =data[1].group_id;
+//						var isUser = false;
+//							for (var i = 0; i < arr.length - 1; i++) {
+//								if (arr[i] == "ROLE_USER") {
+//										isUser = true;
+//								}
+//							}
+//							if(isUser == false){
+//								$("#selfAssessment").attr("disabled",
+//										true);
+//								$("input").each(function(){
+//									$(this).attr("disabled",
+//											true);
+//								});
+//							}
+					} 
+				});
+	}
